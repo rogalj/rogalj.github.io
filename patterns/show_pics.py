85f280c0ad4b0ccec2e29e942307bd6d9ae73dde
rogalj <jutta.rogal@gmail.com>
2025-02-09 18:56:32 -0500

    JR: update pattern webpage

--- patterns/show_pics.py
+++ patterns/show_pics.py
@@ -4,26 +4,7 @@ import streamlit as st
 st.write("# Training dataset checkerboard")
 st.markdown("""---""")
 
-#path = "/Users/jrogal/work/NYU/dmref/outreach/mlforkids/patterns/train_checker/"
-#path = "https://github.com/rogalj/rogalj.github.io/tree/main/patterns/train_checker/"
 path = "https://raw.githubusercontent.com/rogalj/rogalj.github.io/main/patterns/train_checker/"
-
-#imgs = ["img_checker-000.png", "img_checker-001.png", "img_checker-002.png",
-#        "img_checker-003.png", "img_checker-004.png", "img_checker-005.png",
-#        "img_checker-006.png", "img_checker-007.png", "img_checker-008.png"]
-
-
-#for img in imgs:
-#    capt = "Training image " + img
-#    st.image(path+img, caption=capt)
-
-
-#img1 = path+imgs[0]
-#img2 = path+imgs[1]
-
-#img1 = [path+imgs[0], path+imgs[1], path+imgs[2],
-#        path+imgs[3], path+imgs[4], path+imgs[5],
-#        path+imgs[6], path+imgs[7], path+imgs[8]]
 
 caps = []
 imgs = []
@@ -35,16 +16,12 @@ for i in range(50):
 
 
 
-#st.image([img1,img2], caption=[imgs[0],imgs[1]])
-
 col=st.columns(5, gap="medium")
 
 nimg = len(imgs)
 ncol = len(col)
 nimgcol = int(nimg/ncol)
 nleftover = nimg % ncol
-#st.write(nimgcol)
-#st.write(nleftover)
 
 
 start=0
@@ -62,5 +39,3 @@ for i in range(len(col)):
     stop = start + nimgcol
 
 
-#with col[1]:
-#    st.image(img1, caption=imgs)
